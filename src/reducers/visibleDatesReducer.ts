6fd6e624e29debc9d53a884180c2398fa01ee8f5
rowlandwilliams <rowland.williams.94@gmail.com>
2021-08-13 10:25:49 +0100

    add reducer aciton type

--- src/reducers/visibleDatesReducer.ts
+++ src/reducers/visibleDatesReducer.ts
@@ -1,4 +1,6 @@
-export const visibleDatesReducer = (state = [100, 100], action: any) => {
+import { AnyAction } from "redux";
+
+export const visibleDatesReducer = (state = [100, 100], action: AnyAction) => {
   switch (action.type) {
     case "CHANGEDOMAIN":
       return state.map((x) => x + 1);
